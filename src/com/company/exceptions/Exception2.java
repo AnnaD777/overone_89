c9d3d377820616b17d4373b689fc4a0fd828b58f
Anna <KuzmichAnnaNikolaevna@yandex.by>
2022-01-16 16:29:41 +0300

    Add...

---
+++ src/com/company/exceptions/Exception2.java
@@ -0,0 +1,16 @@
+package com.company.exceptions;
+
+public class Exception2 {
+    double divNumbers (int a, int b) {
+        double result = 0;
+
+        try {
+            result=a/b;
+        } catch (ArithmeticException e){
+            System.out.println("Div by zero");
+        } finally {
+            result=12345;
+        }
+        return result;
+    }
+}
